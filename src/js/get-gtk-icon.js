aa9a34f1eaa17513dc5b47d2fbb483a72295fc60
Joshua Gleitze <dev@joshuagleitze.de>
2017-09-18 20:49:00 +0200

    Implementation of hasGtk3

--- src/js/get-gtk-icon.js
+++ src/js/get-gtk-icon.js
@@ -1,10 +1,43 @@
+const child_process = require('child_process');
+
 let cppgeticon;
 let searchedGtk3 = false;
 let foundGtk3 = true;
+let gtkSearchPromise;
 
-function hasGtk3() {
+function hasGtk3(callback) {
 	if (!searchedGtk3) {
+		// Node.js is single-threaded so this is not a race condition.
 		searchedGtk3 = true;
+		gtkSearchPromise = new Promise((resolve, reject) => {
+			child_process.spawn('pkg-config', ['gtk+-3.0'])
+				.on('exit', code => {
+					foundGtk3 = code === 0;
+					resolve(foundGtk3);
+				})
+		});
+	}
+	if (gtkSearchPromise) {
+		const callbacktype = typeof callback;
+		if (callbacktype === 'function') {
+			gtkSearchPromise.then(result => callback(null, result), error => callback(error));
+			return undefined;
+		} else if (callbacktype === 'undefined') {
+			return gtkSearchPromise;
+		} else {
+			throw new Error('The provided callback was not a function!');
+		}
+	} else {
+		return Promise.resolve(foundGtk3);
+	}
+}
+
+function hasGtk3Sync() {
+	if (!searchedGtk3) {
+		// if there’s already a pending async call, we will do the work two times.
+		// But clients really shouldn’t do that, should they?
+		searchedGtk3 = true;
+		foundGtk3 = child_process.spawnSync('pkg-config', ['gtk+-3.0']).status === 0
 	}
 	return foundGtk3;
 }
@@ -14,13 +47,14 @@ function getIconPath(name, size, callback) {
 	checkGtk();
 
 	const callbacktype = typeof callback;
+	const iconPromise = new Promise((resolve, reject) => {
+		cppgeticon.getIcon(name, size, wrap(resolve, reject));
+	});
 	if (callbacktype === 'function') {
-		cppgeticon.getIcon(name, size, wrapSimple(callback));
+		iconPromise.then(result => callback(null, result), error => callback(error));
 		return undefined;
 	} else if (callbacktype === 'undefined') {
-		return new Promise((resolve, reject) => {
-			cppgeticon.getIcon(name, size, wrap(resolve, reject));
-		});
+		return iconPromise;
 	} else {
 		throw new Error('The third argument, if given, must be a callback!');
 	}
@@ -28,17 +62,6 @@ function getIconPath(name, size, callback) {
 
 // callbacks must be called with a clear stack. Promise callbacks will fail
 // otherwise.
-
-function wrapSimple(callback) {
-	return result => setImmediate(() => {
-		if (result === null) {
-			callback(new Error("No icon found"), null);
-		} else {
-			callback(null, result);
-		}
-	})
-}
-
 function wrap(resolve, reject) {
 	return result => setImmediate(() => {
 		if (result === null) {
@@ -75,5 +98,6 @@ function checkGtk() {
 module.exports = {
 	hasGtk3,
 	getIconPath,
+	hasGtk3Sync,
 	getIconPathSync
 }
